86cfa4de0adbe55e1c0d2ae8f6f5e3bae9780956
Pratyush Vishal <pratyushvishal04@gmail.com>
2021-01-15 12:53:46 +0530

    Add hooks

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function CartItem() {
 
@@ -9,24 +9,50 @@ export default function CartItem() {
             borderRadius: 4
         }
     }
+    const [title, setTitle] = useState('');
+    const [price, setPrice] = useState('');
+    const [qty, setQty] = useState(1);
+    const [img, setImg] = useState('');
+
+
 
     return (
         <div className="cart-item">
             <div className="left-block">
                 <img style={styles.image} />
             </div>
-            <div className="right-block">
-                <div>
-                    The Atelier Tailored Coat
-                    </div>
-                <div>$499.00</div>
-                <div>Qty: 1</div>
+            <div className="right-block" >
+                <div style={{ fontSize: 25 }}>
+                    {title}
+                </div>
+                <div style={{ color: '#777' }} >{price}</div>
+                <div style={{ color: '#777' }} onClick={() => setQty(qty + 1)}>Qty: {qty} </div>
                 <div className="cart-items-actions">
+                    <div className="flex space-between">
 
+                        <img style={{ height: 25, width: 25 }}
+                            alt="increase"
+                            src="https://image.flaticon.com/icons/svg/1665/1665578.svg"
+                            className="action-icons"
+                            onClick={() => setQty(qty + 1)}
+                        />
+                        <img style={{ height: 25, width: 25 }}
+                            alt="decrease"
+                            src="https://image.flaticon.com/icons/svg/1665/1665612.svg"
+                            className="action-icons"
+                            onClick={() => setQty(qty - 1)}
+                        />
+                        <img style={{ height: 25, width: 25 }}
+                            alt="delete"
+                            src="https://image.flaticon.com/icons/svg/1214/1214428.svg"
+                            className="action-icons"
+                        // onClick={() => this.props.onDeleteProduct(product.id)}
+                        />
+                    </div>
                 </div>
             </div>
 
-        </div>
+        </div >
     );
 }
 
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 
 
 export default function Project() {
 
+    const [title, setTitle] = useState('The Atelier Tailored Coat');
+    const [price, setPrice] = useState('499');
+    const [qty, setQty] = useState(1);
+    const [img, setImg] = useState('bonfire.jpg');
+
+    function onClickAdd() {
+        setTitle(title);
+        setPrice(price);
+        setQty(qty);
+        setImg(img);
+    }
     return (
         <main className="bg-yellow-100 min-h-screen p-12">
             <section className="container mx-auto grid grid-cols-2 gap-8">
@@ -12,6 +23,22 @@ export default function Project() {
                 <div className="text-gray-500 text-xs space-x-4">
                     <h4 className="text-lg text-black-800 flex justify-center mb-3">The Atelier Tailored Coat</h4>
                     <h3 className="text-lg text-blue-600 flex justify-center">$499.00</h3>
+                    <div style={{ color: '#777' }} onClick={() => setQty(qty + 1)}>Qty: {qty} </div>
+                    <div className="flex space-between">
+                        <img className="mx-3" style={{ height: 25, width: 25 }}
+                            alt="increase"
+                            src="https://image.flaticon.com/icons/svg/1665/1665578.svg"
+                            className="action-icons"
+                            onClick={() => setQty(qty + 1)}
+                        />
+                        <img className="mx-3" style={{ height: 25, width: 25 }}
+                            alt="decrease"
+                            src="https://image.flaticon.com/icons/svg/1665/1665612.svg"
+                            className="action-icons"
+                            onClick={() => setQty(qty - 1)}
+                        />
+                    </div >
+                    <button className="mt-3" onClick={onClickAdd}>ADD TO CART</button>
                 </div>
 
             </section>
